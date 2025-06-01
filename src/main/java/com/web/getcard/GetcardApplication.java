74065f91ca83f858b03c3b0fcddf1924adb786ed
maelfreitas <ripepierre2@gmail.com>
2025-06-01 14:21:06 -0300

    criando projeto

---
+++ src/main/java/com/web/getcard/GetcardApplication.java
@@ -0,0 +1,13 @@
+package com.web.getcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/getcard/GetcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.getcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
